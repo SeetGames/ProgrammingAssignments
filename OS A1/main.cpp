ce0596826d056d6e4563b5b710b1410ef6c86b65
Seet <seetoh.w@digipen.edu>
2023-09-06 05:39:18 +0800

    fk la ushell

---
+++ OS A1/main.cpp
@@ -0,0 +1,23 @@
+#include "uShell.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    bool verboseMode = false;
+
+    // Check command line arguments for verbose mode
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string arg = argv[i];
+        if (arg == "-v" || arg == "--verbose")
+        {
+            verboseMode = true;
+        }
+    }
+
+    uShell shell(verboseMode);
+    int exitCode = shell.run();
+
+    std::cout << "Exiting with code: " << exitCode << std::endl;
+    return exitCode;
+}
--- OS A1/uShell.cpp
+++ OS A1/uShell.cpp
@@ -1,95 +1,207 @@
 #include "uShell.h"
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 
-// Implementation of constructor
-uShell::uShell(bool bFlag)
-    : m_prompt("$ "), m_verbose(bFlag), m_exitCode(0)
+// Constructor
+uShell::uShell(bool bFlag) 
+: m_prompt("uShell>"), 
+m_verbose(bFlag), 
+m_exitFlag(false), 
+m_exitCode(0)
 {
-    // Initialize internal command map here if needed
+    m_internalCmdList["echo"] = &uShell::echo;
+    m_internalCmdList["setVar"] = &uShell::setVar;
+    m_vars["PATH"] = ""; // Initialize PATH variable or set to a default value
 }
 
-bool uShell::getInput(std::string &input) const {
-    std::getline(std::cin, input);
+bool uShell::getInput(std::string& _input) const
+{
+    std::getline(std::cin, _input);
+    if (_input.back() == '\r')
+        _input.pop_back();
     return !std::cin.eof();
 }
 
-void uShell::printVerbose(std::string const &input) {
-    if (m_verbose) {
-        std::cout << input << std::endl;
-    }
+void uShell::printVerbose(std::string const& _input)
+{
+    auto start = _input.find_first_not_of(' ');
+    auto end = _input.find_last_not_of(' ');
+    if (start != std::string::npos && end != std::string::npos)
+        std::cout << _input.substr(start, end - start + 1) << std::endl;
 }
 
-void uShell::tokenize(std::string const &input, TokenList &tokenList) const {
-    std::istringstream iss(input);
+void uShell::tokenize(std::string const& _input, TokenList& _tokenList) const
+{
+    std::istringstream iss(_input);
     std::string token;
-    while (iss >> token) {
-        tokenList.push_back(token);
-    }
+    while (iss >> token)
+        _tokenList.push_back(token);
 }
 
-bool uShell::replaceVars(TokenList &tokenList) const {
-    // Implement variable replacement logic here
-    return true;
-}
-
-bool uShell::isValidVarname(char const *start, char const *end) const {
-    // Implement variable name validation logic here
-    return true;
-}
-
-std::string uShell::mergeTokens(TokenList const &tokenList, unsigned startPos) const {
-    std::ostringstream oss;
-    for (unsigned i = startPos; i < tokenList.size(); ++i) {
-        if (i > startPos) {
-            oss << ' ';
+bool uShell::replaceVars(TokenList& _tokenList) const
+{
+    for (auto& token : _tokenList)
+    {
+        // Check for comment sign and remove all following tokens
+        if (token.find("#") != std::string::npos)
+        {
+            _tokenList.erase(std::remove_if(_tokenList.begin(), _tokenList.end(),
+                [&](const std::string& t) { return t.find("#") != std::string::npos; }),
+                _tokenList.end());
+            break;
         }
-        oss << tokenList[i];
+
+        size_t startPos = token.find("${");
+        size_t endPos = token.find("}", startPos);
+        while (startPos != std::string::npos && endPos != std::string::npos)
+        {
+            std::string varName = token.substr(startPos + 2, endPos - startPos - 2);
+            
+            // Check for valid variable name
+            if (!isValidVarname(varName.c_str(), varName.c_str() + varName.size()))
+            {
+                std::cerr << "Error: Invalid variable name " << varName << std::endl;
+                return false;
+            }
+
+            try 
+            {
+                token.replace(startPos, endPos - startPos + 1, m_vars.at(varName));
+            } 
+            catch (const std::out_of_range&) 
+            {
+                std::cerr << "Error: Variable " << varName << " not found." << std::endl;
+                return false;
+            }
+            startPos = token.find("${", endPos);
+            endPos = token.find("}", startPos);
+        }
     }
+    return true;
+}
+
+bool uShell::isValidVarname(char const* _start, char const* _end) const
+{
+    if (!std::isalpha(*_start))
+    {
+        return false;
+    }
+    _start++;
+    while (_start != _end)
+    {
+        if (!std::isalnum(*_start))
+        {
+            return false;
+        }
+        _start++;
+    }
+    return true;
+}
+
+std::string uShell::mergeTokens(TokenList const& _tokenList, unsigned _startPos) const
+{
+    std::ostringstream oss;
+    for (unsigned i = _startPos; i < _tokenList.size(); ++i)
+    {
+        oss << _tokenList[i];
+        if (i != _tokenList.size() - 1)  // Check if it's not the last token
+        {
+            oss << " ";
+        }
+    }
+    std::string merged = mergeTokens(_tokenList, 0);
+    std::cout << "Merged tokens: [" << merged << "]" << std::endl;
     return oss.str();
 }
 
-void uShell::echo(TokenList const &tokenList) {
-    std::string output = mergeTokens(tokenList, 1);
-    std::cout << output << std::endl;
-}
-
-void uShell::setVar(TokenList const &tokenList) {
-    if (tokenList.size() >= 3) {
-        std::string key = tokenList[1];
-        std::string value = mergeTokens(tokenList, 2);
-        m_vars[key] = value;
-    }
-}
-
-void removeComments(std::string &input) 
+void uShell::echo(TokenList const& _tokenList)
 {
-    auto pos = input.find("#");
-    if (pos != std::string::npos) {
-        input = input.substr(0, pos);
+    for (unsigned i = 1; i < _tokenList.size(); ++i)
+    {
+        std::cout << _tokenList[i];
+        if (i != _tokenList.size() - 1)  // Check if it's not the last token
+        {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+void uShell::setVar(TokenList const& _tokenList)
+{
+    if (_tokenList.size() < 2)
+    {
+        std::cerr << "Error: Invalid setVar command." << std::endl;
+        return;
+    }
+    std::string varName = _tokenList[1];
+    std::string value = (_tokenList.size() > 2) ? mergeTokens(_tokenList, 2) : "";
+    if (isValidVarname(varName.c_str(), varName.c_str() + varName.size()))
+    {
+        m_vars[varName] = value;
+    }
+    else
+    {
+        std::cerr << "Error: Invalid variable name." << std::endl;
     }
 }
 
-int uShell::run() {
-    std::string input;
-    while (true) {
-        std::cout << m_prompt;
-        if (!getInput(input)) break;
+int uShell::run()
+{
+    while (!m_exitFlag)
+    {
+        std::cout << m_prompt << " ";  // Use the member variable for the prompt
+        std::string input;
+        if (getInput(input))
+        {
+            TokenList tokens;
+            tokenize(input, tokens);
+            if (m_verbose)
+                printVerbose(input);
+            if (!replaceVars(tokens))
+                continue;
+            if (tokens.empty())
+                continue;
+            if (m_internalCmdList.find(tokens[0]) != m_internalCmdList.end())
+                (this->*m_internalCmdList[tokens[0]])(tokens);
+            else
+            {
+                // External command handling can be added here
+            }
 
-        removeComments(input);  // Call the member function
-        printVerbose(input);
+            // Check if the first token is the "exit" command
+            if (tokens[0] == "exit")
+            {
+                if (tokens.size() > 2)
+                {
+                    std::cerr << "Error: Too many arguments for exit command." << std::endl;
+                    continue;
+                }
 
-        TokenList tokenList;
-        tokenize(input, tokenList);
-        if (!tokenList.empty()) {
-            const auto &cmd = tokenList[0];
-            if (m_internalCmdList.find(cmd) != m_internalCmdList.end()) {
-                auto func = m_internalCmdList[cmd];
-                (this->*func)(tokenList);
-            } else {
-                // Handle external commands here
+                if (tokens.size() == 2)
+                {
+                    try
+                    {
+                        m_exitCode = std::stoi(tokens[1]);
+                    }
+                    catch (const std::exception&)
+                    {
+                        std::cerr << "Error: Invalid exit code." << std::endl;
+                        continue;
+                    }
+                }
+                else
+                {
+                    m_exitCode = 0;  // Default exit value
+                }
+
+                m_exitFlag = true;  // Set the flag to terminate the loop
+                break;  // Exit the loop immediately
             }
         }
+        else
+            break;
     }
     return m_exitCode;
 }
