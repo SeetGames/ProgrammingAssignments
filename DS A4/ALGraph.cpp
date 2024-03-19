875482c92cb5c2dd19fd1ddb2eb5dfa7b3759f6a
Seet <weitungseetoh@gmail.com>
2024-03-20 03:11:57 +0800

    update

--- DS A4/ALGraph.cpp
+++ DS A4/ALGraph.cpp
@@ -1,23 +1,48 @@
+/*!*************************************************************************
+\file ALGraph.cpp
+\author Seetoh Wei Tung
+\par DP email: seetoh.w@digipen.edu
+\par Course: Data Structures
+\par Assignment 4
+\date 18-03-2024
+\brief
+This file contains the implementation for the ALGraph
+***************************************************************************/
+
 #include "ALGraph.h"
 #include <algorithm>
 #include <queue>
 
+
+/**
+ * Constructor for the ALGraph class
+ * @param size The number of nodes in the graph
+ */
 ALGraph::ALGraph(unsigned size)
 {
-  list_.reserve(size);
+  m_AdjacencyList.reserve(size);
   for (size_t i = 0; i < size; ++i)
-    list_.push_back(ALIST::value_type());
+    m_AdjacencyList.push_back(ALIST::value_type());
 }
 
+/**
+ * Destructor for the ALGraph class
+ */
 ALGraph::~ALGraph(void)
 {
 }
 
+/**
+ * Add a directed edge to the graph
+ * @param source The source node
+ * @param destination The destination node
+ * @param weight The weight of the edge
+ */
 void ALGraph::AddDEdge(unsigned source, unsigned destination, unsigned weight)
 {
   unsigned id = source - 1;
   AdjacencyInfo info{destination, weight};
-  auto &adj_list = list_.at(id);
+  auto &adj_list = m_AdjacencyList.at(id);
 
   for (auto i = adj_list.begin(); i != adj_list.end(); ++i)
   {
@@ -35,41 +60,48 @@ void ALGraph::AddDEdge(unsigned source, unsigned destination, unsigned weight)
   adj_list.push_back(info);
 }
 
+/**
+ * Add an undirected edge to the graph
+ * @param node1 One node of the edge
+ * @param node2 The other node of the edge
+ * @param weight The weight of the edge
+ */
 void ALGraph::AddUEdge(unsigned node1, unsigned node2, unsigned weight)
 {
   AddDEdge(node1, node2, weight);
   AddDEdge(node2, node1, weight);
 }
 
+
 std::vector<DijkstraInfo> ALGraph::Dijkstra(unsigned start_node) const
 {
-  auto GetAdjList = [=](unsigned i) { return list_.at(i - 1); };
+  auto GetAdjList = [=](unsigned i) { return m_AdjacencyList.at(i - 1); };
   auto comp = [](const GNode *lhs, const GNode *rhs) -> bool { return *lhs < *rhs; };
 
   std::priority_queue<GNode *, std::vector<GNode *>, decltype(comp)> pq(comp);
   std::vector<DijkstraInfo> result;
   std::vector<GNode> nodes;
 
-  result.reserve(list_.size());
-  nodes.reserve(list_.size());
+  result.reserve(m_AdjacencyList.size());
+  nodes.reserve(m_AdjacencyList.size());
 
   //initialize all nodes in graph
-  for (unsigned i = 1; i < list_.size() + 1; ++i)
+  for (unsigned i = 1; i < m_AdjacencyList.size() + 1; ++i)
   {
     GNode node;
     if (i != start_node)
     {
-      node.evaluated = false;
-      node.info.cost = INFINITY_;
+      node.m_Evaluated = false;
+      node.info.cost = INF;
     }
     else
     {
-      node.evaluated = true;
+      node.m_Evaluated = true;
       node.info.cost = 0;
       node.info.path.push_back(start_node);
     }
 
-    node.id = i;
+    node.m_id = i;
     nodes.push_back(node);
   }
 
@@ -92,10 +124,10 @@ std::vector<DijkstraInfo> ALGraph::Dijkstra(unsigned start_node) const
     GNode *v = pq.top();
     pq.pop();
 
-    v->evaluated = true;
-    v->info.path.push_back(v->id);
+    v->m_Evaluated = true;
+    v->info.path.push_back(v->m_id);
 
-    const auto &adj_list = GetAdjList(v->id);
+    const auto &adj_list = GetAdjList(v->m_id);
 
     for (unsigned i = 0; i < adj_list.size(); ++i)
     {
@@ -109,8 +141,8 @@ std::vector<DijkstraInfo> ALGraph::Dijkstra(unsigned start_node) const
         u.info.cost = new_cost;
         u.info.path = v->info.path;
 
-        if (!v->evaluated)
-          u.info.path.push_back(v->id);
+        if (!v->m_Evaluated)
+          u.info.path.push_back(v->m_id);
 
         pq.push(&u);
       }
@@ -124,10 +156,15 @@ std::vector<DijkstraInfo> ALGraph::Dijkstra(unsigned start_node) const
   return result;
 }
 
+/**
+ * Get the adjacency list representation of the graph
+ * @return The adjacency list
+ */
 ALIST ALGraph::GetAList(void) const
 {
-  return list_;
+  return m_AdjacencyList;
 }
+
 
 bool ALGraph::GNode::operator<(const GNode &rhs) const
 {
--- DS A4/ALGraph.h
+++ DS A4/ALGraph.h
@@ -1,8 +1,19 @@
+/*!*************************************************************************
+\file ALGraph.h
+\author Seetoh Wei Tung
+\par DP email: seetoh.w@digipen.edu
+\par Course: Data Structures
+\par Assignment 4
+\date 18-03-2024
+\brief
+This file contains the declaration for the ALGraph
+***************************************************************************/
 //---------------------------------------------------------------------------
 #ifndef ALGRAPH_H
 #define ALGRAPH_H
 //---------------------------------------------------------------------------
 #include <vector>
+#include <limits>
 
 struct DijkstraInfo
 {
@@ -16,36 +27,43 @@ struct AdjacencyInfo
   unsigned weight;
 };
 
-typedef std::vector<std::vector<AdjacencyInfo> > ALIST;
+typedef std::vector<std::vector<AdjacencyInfo>> ALIST;
 
 class ALGraph
 {
-  public:
-    ALGraph(unsigned size);
-    ~ALGraph(void);
-    void AddDEdge(unsigned source, unsigned destination, unsigned weight);
-    void AddUEdge(unsigned node1, unsigned node2, unsigned weight);
+public:
+  ALGraph(unsigned size);
+  ~ALGraph(void);
+  void AddDEdge(unsigned source, unsigned destination, unsigned weight);
+  void AddUEdge(unsigned node1, unsigned node2, unsigned weight);
 
-    std::vector<DijkstraInfo> Dijkstra(unsigned start_node) const;
-    ALIST GetAList(void) const;
-        
-  private:
-    
-    // An EXAMPLE of some other classes you may want to create and 
-    // implement in ALGraph.cpp
-    class GNode; 
-    class GEdge;
-    struct AdjInfo
-    {
-      GNode *node;
-      unsigned weight;
-      unsigned cost;
-      AdjInfo();
-      bool operator<(const AdjInfo& rhs) const;
-      bool operator>(const AdjInfo& rhs) const;
-    };
-    
-    // Other private fields and methods
+  std::vector<DijkstraInfo> Dijkstra(unsigned start_node) const;
+  ALIST GetAList(void) const;
+
+private:
+  // An EXAMPLE of some other classes you may want to create and
+  // implement in ALGraph.cpp
+  struct GNode
+  {
+    unsigned m_id;
+    bool m_Evaluated;
+    DijkstraInfo info;
+    bool operator<(const GNode &rhs) const;
+  };
+  class GEdge;
+  struct AdjInfo
+  {
+    //unsigned node;
+    unsigned weight;
+    unsigned cost;
+    //AdjInfo();
+    //bool operator<(const AdjInfo &rhs) const;
+    //bool operator>(const AdjInfo &rhs) const;
+  };
+
+  // Other private fields and methods
+  ALIST m_AdjacencyList;
+  const unsigned INF = static_cast<unsigned>(-1);
 };
 
 #endif
